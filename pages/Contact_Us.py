b9c0ab33b42cba623e6c0a158906244cf3269d0b
Togay Tunca <tuncatogay@gmail.com>
2023-01-01 20:12:52 +0300

    Send email

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from send_email import send_email
 
 
 st.header("Contact Me")
@@ -6,7 +7,15 @@ st.header("Contact Me")
 with st.form(key="email_form"):
     user_email = st.text_input("Your Email Address")
     raw_message = st.text_area("Text")
+    message = f"""\
+Subject: New email from {user_email}
+
+From: {user_email}
+{raw_message}
+"""
 
     button = st.form_submit_button("Submit")
+    print(button)
     if button:
-        print("I was pressed")
+        send_email(message)
+        st.info("Your email was sent successfully")
--- send_email.py
+++ send_email.py
@@ -1,20 +1,18 @@
 import smtplib, ssl
-
-host = "smtp.gmail.com"
-port = 465
-
-username = "ttunca@evin-ai.com"
-password = "htcolqlotwzowlyj"
-
-receiver = "ttunca@evin-ai.com"
-context = ssl.create_default_context()
+import os
 
 
-message = """\
-Subject: Hi!,
-How are You?
-Bye
-"""
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "ttunca@evin-ai.com"
+    password = os.getenv("EMAIL_PASSWORD_PORTFOLIO")
+
+    receiver = "ttunca@evin-ai.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+
